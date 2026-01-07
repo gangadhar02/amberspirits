1d83132ffe65f64a0a0812e4221f252f20a17112
Gangadhar S <sgangadhar.exe@gmail.com>
2026-01-07 21:01:01 +0530

    Fix: Remove grey background from Featured Brands image on mobile

--- src/components/Home/FeaturedBrands.jsx
+++ src/components/Home/FeaturedBrands.jsx
@@ -34,7 +34,7 @@ const FeaturedBrands = () => {
                             transition={{ duration: 0.5 }}
                             className="w-full md:w-1/2 flex justify-center"
                         >
-                            <div className="relative w-64 h-96 md:w-80 md:h-[500px] bg-dark-elevated rounded-full flex items-center justify-center border border-white/5 shadow-2xl" style={{ shadowColor: brands[currentIndex].color }}>
+                            <div className="relative w-64 h-96 md:w-80 md:h-[500px] md:bg-dark-elevated md:rounded-full flex items-center justify-center md:border md:border-white/5 md:shadow-2xl" style={{ shadowColor: brands[currentIndex].color }}>
                                 <img
                                     src={brands[currentIndex].image}
                                     alt={brands[currentIndex].name}
